a3b27f269a43b5409d40254935648f546abbbfdb
malyokhova <alyokhova@gmail.com>
2023-06-19 17:46:02 +0300

    Initial commit

---
+++ lab1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab1/lab1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			float a = 10, b = 5;
+			float sum = a + b;
+			Assert::AreEqual(sum, Sum(a, b));
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			float a = 14, b = 12;
+			float sub = a - b;
+			Assert::AreEqual(sub, Subtraction(a, b));
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			float a = 10, b = 5;
+			float multipl = a * b;
+			Assert::AreEqual(multipl, Multiplication(a, b));
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			float a = 48, b = 8;
+			float div = a / b;
+			Assert::AreEqual(div, Division(a, b));
+		}
+	};
+}
---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+float Sum(float, float);
+float Subtraction(float, float);
+float Multiplication(float, float);
+float Division(float, float);
+
+int main()
+{
+    float a, b;
+    int operation;
+    cout << "Enter 2 numbers: " << endl;
+    cin >> a >> b;
+
+    cout << "\n Select the operation:" << endl <<
+        "1 - Addition" << endl <<
+        "2 - Subtraction" << endl <<
+        "3 - Multiplication" << endl <<
+        "4 - Division" << endl;
+
+    cin >> operation;
+
+    switch (operation)
+    {
+    case 1:
+        cout << "Answer: " << Sum(a, b) << endl;
+        break;
+    case 2:
+        cout << "Answer: " << Subtraction(a, b) << endl;
+        break;
+    case 3:
+        cout << "Answer: " << Multiplication(a, b) << endl;
+        break;
+    case 4:
+        if (b != 0)
+        {
+            cout << "Answer: " << Division(a, b) << endl;
+        }
+        else
+        {
+            cout << "Division by 0" << endl;
+        }
+        break;
+    default:
+        cout << "There is no such operation" << endl;
+    }
+
+    return 0;
+}
+
+float Sum(float num1, float num2)
+{
+    return num1 + num2;
+}
+
+float Subtraction(float num1, float num2)
+{
+    return num1 - num2;
+}
+
+float Multiplication(float num1, float num2)
+{
+    return num1 * num2;
+}
+
+float Division(float num1, float num2)
+{
+    return num1 / num2;
+}
